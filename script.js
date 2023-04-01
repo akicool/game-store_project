87efd9e920ff7530ed02e16635c268681b0287ae
David <liid05754@gmail.com>
2023-04-02 02:51:16 +0300

    added sign in and log in block

--- script.js
+++ script.js
@@ -10,3 +10,55 @@ var swiper = new Swiper(".mySwiper", {
     mousewheel: true,
     keyboard: true,
   });
+
+// main page
+let navUser = document.querySelector('.nav__user');
+let dialog = document.querySelector('.dialog');
+let dialogWindow = document.querySelector('.dialog__window');
+let subContainer = document.querySelector('.sub-container');
+let container = document.querySelector('.container');
+
+// selection page 
+let dialogButtonBack = document.querySelector('.dialog__button-back');
+let dialogButtonSignin = document.querySelector('.dialog__button-signin');
+let dialogButtonLogin = document.querySelector('.dialog__button-login ');
+let formSignin = document.querySelector('.form__signin');
+let formLogin = document.querySelector('.form__login')
+
+// signin block 
+let formSigninButtonBack = document.querySelector('.form__signin-button-back');
+
+// login block 
+let formLoginButtonBack = document.querySelector('.form__login-button-back');
+
+navUser.onclick = function() {
+  container.classList.add('active')
+  subContainer.classList.add('active')
+  dialog.classList.add('active');
+};  
+
+dialogButtonBack.onclick = function() {
+  container.classList.remove('active');
+  subContainer.classList.remove('active');
+  dialog.classList.remove('active');
+};
+
+dialogButtonSignin.onclick = function() {
+  dialogWindow.classList.add('remove-block');
+  formSignin.classList.add('active');
+};
+
+formSigninButtonBack.onclick = function() {
+  formSignin.classList.remove('active');
+  dialogWindow.classList.remove('remove-block');
+};
+
+dialogButtonLogin.onclick = function() {
+  dialogWindow.classList.add('remove-block');
+  formLogin.classList.add('active');
+};
+
+formLoginButtonBack.onclick = function() {
+  formLogin.classList.remove('active');
+  dialogWindow.classList.remove('remove-block');
+};
